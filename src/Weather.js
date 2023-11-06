d88fa202fba7bd3c6adb77dca5c053a1bb9d8745
NoemiSpadini <noemispadini94@gmail.com>
2023-11-06 11:21:38 +0100

    updates weather app

---
+++ src/Weather.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  const [city, setCity] = useState(null);
+  const [weather, setWeather] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function searchCity(event) {
+    event.preventDefault();
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={searchCity}>
+      <input
+        type="search"
+        placeholder="Search a city..."
+        onChange={updateCity}
+      />
+      <input type="submit" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt="" />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
